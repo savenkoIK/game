7ae424771809d57004627fd4404a20f280c46c58
savenkoIK <savenko.ik@gmail.com>
2020-11-04 20:06:31 +0200

    Update game.js

--- game.js
+++ game.js
@@ -10,7 +10,8 @@ var text;
 var winningMessage;
 var won = false;
 var currentScore = 0;
-var winningScore = 100;
+var winningScore = 105
+;
 
 // add collectable items to the game
 function addItems() {
